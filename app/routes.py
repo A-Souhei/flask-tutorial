2c519584a87811e4910aa6cbd0339b7cf411868b
toavina <toavina@fjelltopp.org>
2022-12-14 16:36:03 +0300

    chapter 5 database - login required / next

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,11 @@ from app.models import User
 from flask_login import logout_user
 
 
+from flask import request
+from werkzeug.urls import url_parse
+
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -39,7 +44,11 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        # redirection after login using next
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
