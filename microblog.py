0a44ad2327730940369d7443a8da8d18fc46bdb9
toavina <toavina@fjelltopp.org>
2022-12-14 15:53:07 +0300

    chapter 4 database - add db,User,Post to shell context

--- microblog.py
+++ microblog.py
@@ -5,3 +5,4 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post} # Important to return the db, User and Post objects
+    # run: flask shell
