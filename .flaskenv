0263486d2ea2cd5048c4b30e34b80fe234011ef9
toavina <toavina@fjelltopp.org>
2022-12-15 10:51:19 +0300

    chapter 7 send error to mail

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,5 @@
 FLASK_APP=microblog.py
+FLASK_DEBUG=off
+FLASK_ENV=production
+MAIL_SERVER=localhost
+MAIL_PORT=8025
--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_migrate import Migrate
 
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler
+
+
 app = Flask(__name__)
 app.config.from_object(Config)  # Important to load the config
 
@@ -13,7 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-from app import routes, models
+from app import routes, models, errors
 
 # run:  flask db init
 # run: flask db migrate
@@ -25,3 +29,21 @@ from app import routes, models
 # run: flask db upgrade
 
 login.login_view = 'login'
+
+# second terminal then run: python -m smtpd -n -c DebuggingServer localhost:8025 | alternative is sendgrid
+# could have done this in SEIE ???
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
