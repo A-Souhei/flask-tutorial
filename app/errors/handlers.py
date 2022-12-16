30a4023feba9e130f232752cdeb6aac1a8ba9ff4
toavina <toavina@fjelltopp.org>
2022-12-16 10:26:36 +0300

    chapter 15 error blueprint

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,12 +3,12 @@ from app import db
 from app.errors import bp # this is the blueprint
 
 
-@bp.app_errorhandle(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
 
-@bp.app_errorhandle(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()  # Important to rollback the session in case of error
     return render_template('500.html'), 500
